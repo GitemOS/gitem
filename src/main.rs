a9cb51f8e0ec53a61ddae233d85be315b0152ddc
Micah Tanner <micahsoftdotexe@gmail.com>
2022-06-16 17:55:52 -0400

    Got a list of packages and started on cli args

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,47 @@
-use tui;
+use alpm::{Alpm, SigLevel};
+use clap::{Parser, Subcommand};
+
+/// Simple program to greet a person
+#[derive(Parser)]
+#[clap(author, version, about, long_about = None)]
+struct Args {
+    #[clap(subcommand)]
+    list: Option<ListCommands>
+}
+
+#[derive(Subcommand)]
+enum ListCommands {
+    Test {
+        #[clap(short, long, action)]
+        all: bool,
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+    if let Some(list) = args.list.unwrap() {
+        println!("His name is {}", name);
+    }
+    else {
+            // let cwd = std::env::current_dir().unwrap();
+        // println!("The cwd path is: {:?}", cwd.as_path());
+        let handle = Alpm::new("/", "/var/lib/pacman/").unwrap();
+
+        handle
+            .register_syncdb("community", SigLevel::USE_DEFAULT)
+            .unwrap();
+        
+        // for db in handle.syncdbs() {
+        //     println!("The db name is: {}", db.name());
+        //     let package_list = db.pkgs();
+        //     println!("{:?}", package_list);
+        // }
+        let local_db = handle.localdb();
+        let local_packages = local_db.pkgs();
+        for pkg in local_packages {
+            println!("Package Name: {} Package Version: {} Arch: {}", pkg.name(), pkg.version(), pkg.arch().unwrap());
+        }
+        // println!("The packages: {:?}", local_packages.len());
+    }
+    
 }
